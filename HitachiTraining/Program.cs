1dfd3b98e0ba063d27ff1f37e80a3207c1c3ad17
defindal <defindal@gmail.com>
2022-06-14 10:37:04 +0700

    WPF time

--- HitachiTraining/Program.cs
+++ HitachiTraining/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
 
 namespace HitachiTraining
 {
@@ -12,10 +13,27 @@ namespace HitachiTraining
             //ExceptionHandling exceptionHandling = new ExceptionHandling();
             //var result = exceptionHandling.cicilanPerBulan(10000, -2);
             //Console.WriteLine(result);
-            LINQPractice.fromJSON();
+            // LINQPractice.fromJSON();
 
-            Console.ReadLine();
+            try
+            {
+                int? x = null;
+                // int y = null;
+                // x = 18;
 
+                Debug.Assert(x.HasValue);
+                Console.WriteLine(x.Value);
+                int z = 6 / x.Value;
+
+            } catch(Exception ex)
+            {
+                // logging
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ MyApplication/ItemViewer.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyApplication
+{
+    /// <summary>
+    /// Interaction logic for ItemViewer.xaml
+    /// </summary>
+    public partial class ItemViewer : Window
+    {
+        public ItemViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            lstNames.Items.Add(txtName.Text);
+            txtName.Clear();
+        }
+    }
+}
